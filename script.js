eafaa3d6a8700d44ca5ab4607421ca9318beff5d
Beatriz Andrade <70031630+Beatriz-andrade09@users.noreply.github.com>
2021-11-20 22:49:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $("#loginAluno").submit(function(event){
+
+    if($("#inputUsuario").val() == "root" && $("#inputSenha").val() == "1234"){
+
+      $("#mensagem").addClass("alert alert-success alert-dismissible fade show")
+
+      $("#")
+    }
+  })
+})
